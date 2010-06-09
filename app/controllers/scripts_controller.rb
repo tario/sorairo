5ac6ba92439575d6e3e56560b92649cb6d4c0b4f
Dario Seminara <rseminara@hotmail.com>
2010-06-09 12:45:46 -0300

    added action for run the scripts

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,4 +1,11 @@
 class ScriptsController < ApplicationController
   scriptffolding :scripts, :content_field => :content
   scriptffolding_button :action => "run", :icon => "/images/play.gif"
+
+  def run
+    s = Script.find(params[:id])
+    s.content = params[:content]
+    #DON'T save the script
+    s.run # run the script
+  end
 end
