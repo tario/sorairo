aff179f520d1863f0ede1903a44eed98d113bf30
Dario <darios3@gmail.com>
2010-06-20 13:14:50 -0300

    active_scaffold and scriptffold combined

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -1,12 +1,13 @@
 class ScriptsController < ApplicationController
-#  scriptffold :scripts, :content_field => :content
- # scriptffold_button :action => "run", :icon => "/images/play.gif"
   active_scaffold :scripts do |config|
     config.label = "Scripts"
     config.columns = [:name]
 #    list.columns.exclude :comments
     list.sorting = {:name => 'ASC'}
   end
+
+  scriptffold :scripts, :content_field => :content
+  scriptffold_button :action => "run", :icon => "/images/play.gif"
 
 
   def run
